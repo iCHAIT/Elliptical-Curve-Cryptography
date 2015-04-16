a366f3aa844c55030ecb7b5ae53bd92e98e7bc09
ameenkhan07 <ameenkhan07@gmail.com>
2015-04-16 19:56:45 +0530

    Initial Commit | -Aman Arora

---
+++ ecdh.py
@@ -0,0 +1,303 @@
+# Basics of Elliptic Curve Cryptography implementation on Python
+from Crypto.Cipher import AES
+from hashlib import md5
+import hashlib
+import base64
+import os
+ 
+import collections
+import base64
+import os
+def inv(n, q):
+    """div on PN modulo a/b mod q as a * inv(b, q) mod q
+    >>> assert n * inv(n, q) % q == 1
+    """
+    return egcd(n, q)[0] % q
+ 
+ 
+def egcd(a, b):
+    """extended GCD
+    returns: (s, t, gcd) as a*s + b*t == gcd
+    >>> s, t, gcd = egcd(a, b)
+    >>> assert a % gcd == 0 and b % gcd == 0
+    >>> assert a * s + b * t == gcd
+    """
+    s0, s1, t0, t1 = 1, 0, 0, 1
+    while b > 0:
+        q, r = divmod(a, b)
+        a, b = b, r
+        s0, s1, t0, t1 = s1, s0 - q * s1, t1, t0 - q * t1
+        pass
+    return s0, t0, a
+ 
+def inv2(n, q):
+    """another PN invmod: from euler totient function
+    - n ** (q - 1) % q = 1 => n ** (q - 2) % q = n ** -1 % q
+    """
+    assert q > 2
+    s, p2, p = 1, n, q - 2
+    while p > 0:
+        if p & 1 == 1: s = s * p2 % q
+        p, p2 = p >> 1, pow(p2, 2, q)
+        pass
+    return s
+ 
+ 
+ 
+ 
+def sqrt(n, q):
+    """sqrtmod for bigint
+    - Algorithm 3.34 of http://www.cacr.math.uwaterloo.ca/hac/about/chap3.pdf
+    """
+    import random
+    # b: some non-quadratic-residue
+    b = 0 
+    while b == 0 or jacobi(b, q) != -1:
+        b = random.randint(1, q - 1)
+        pass
+    # q = t * 2^s + 1, t is odd
+    t, s = q - 1, 0 
+    while t & 1 == 0:
+        t, s = t >> 1, s + 1
+        pass
+    assert q == t * pow(2, s) + 1 and t % 2 == 1
+    ni = inv(n, q)
+    c = pow(b, t, q)
+    r = pow(n, (t + 1) // 2, q)
+    for i in range(1, s):
+        d = pow(pow(r, 2, q) * ni % q, pow(2, s - i - 1, q), q)
+        if d == q - 1: r = r * c % q
+        c = pow(c, 2, q)
+        pass
+    return (r, q - r)
+ 
+ 
+def jacobi(a, q):
+    """jacobi symbol: judge existing sqrtmod (1: exist, -1: not exist)
+    - j(a*b,q) = j(a,q)*j(b,q)
+    - j(a*q+b, q) = j(b, q)
+    - j(a, 1) = 1
+    - j(0, q) = 0
+    - j(2, q) = -1 ** (q^2 - 1)/8
+    - j(p, q) = -1 ^ {(p - 1)/2 * (q - 1)/2} * j(q, p)
+    """
+    if q == 1: return 1
+    if a == 0: return 0
+    if a % 2 == 0: return (-1) ** ((q * q - 1) // 8) * jacobi(a // 2, q)
+    return (-1) ** ((a - 1) // 2 * (q - 1) // 2) * jacobi(q % a, a)
+ 
+def jacobi2(a, q):
+    """quick jacobi symbol
+    - algorithm 2.149 of http://www.cacr.math.uwaterloo.ca/hac/about/chap2.pdf
+    """
+    if a == 0: return 0
+    if a == 1: return 1
+    a1, e = a, 0
+    while a1 & 1 == 0:
+        a1, e = a1 >> 1, e + 1
+        pass
+    m8 = q % 8
+    s = -1 if m8 == 3 or m8 == 5 else 1 # m8 = 0,2,4,6 and 1,7
+    if q % 4 == 3 and a1 % 4 == 3: s = -s
+    return s if a1 == 1 else s * jacobi2(q % a1, a1)
+
+Coord = collections.namedtuple("Coord", ["x", "y"])
+ 
+ 
+class EC(object):
+    """System of Elliptic Curve"""
+    def __init__(self, a, b, q):
+        """elliptic curve as: (y**2 = x**3 + a * x + b) mod q
+        - a, b: params of curve formula
+        - q: prime number
+        """
+        assert 0 < a and a < q and 0 < b and b < q and q > 2
+        assert (4 * (a ** 3) + 27 * (b ** 2))  % q != 0
+        self.a = a
+        self.b = b
+        self.q = q
+        # just as unique ZERO value representation for "add": (not on curve)
+        self.zero = Coord(0, 0)
+        pass
+ 
+    def is_valid(self, p):
+        if p == self.zero: return True
+        l = (p.y ** 2) % self.q
+        r = ((p.x ** 3) + self.a * p.x + self.b) % self.q
+        return l == r
+ 
+    def at(self, x):
+        """find points on curve at x
+        - x: int < q
+        - returns: ((x, y), (x,-y)) or not found exception
+        >>> a, ma = ec.at(x)
+        >>> assert a.x == ma.x and a.x == x
+        >>> assert a.x == ma.x and a.x == x
+        >>> assert ec.neg(a) == ma
+        >>> assert ec.is_valid(a) and ec.is_valid(ma)
+        """
+        assert x < self.q
+        ysq = (x ** 3 + self.a * x + self.b) % self.q
+        y, my = sqrt(ysq, self.q)
+        return Coord(x, y), Coord(x, my)
+ 
+    def neg(self, p):
+        """negate p
+        >>> assert ec.is_valid(ec.neg(p))
+        """
+        return Coord(p.x, -p.y % self.q)
+ 
+    def add(self, p1, p2):
+        """<add> of elliptic curve: negate of 3rd cross point of (p1,p2) line
+        >>> d = ec.add(a, b)
+        >>> assert ec.is_valid(d)
+        >>> assert ec.add(d, ec.neg(b)) == a
+        >>> assert ec.add(a, ec.neg(a)) == ec.zero
+        >>> assert ec.add(a, b) == ec.add(b, a)
+        >>> assert ec.add(a, ec.add(b, c)) == ec.add(ec.add(a, b), c)
+        """
+        if p1 == self.zero: return p2
+        if p2 == self.zero: return p1
+        if p1.x == p2.x and (p1.y != p2.y or p1.y == 0):
+            # p1 + -p1 == 0
+            return self.zero
+        if p1.x == p2.x:
+            # p1 + p1: use tangent line of p1 as (p1,p1) line
+            l = (3 * p1.x * p1.x + self.a) * inv(2 * p1.y, self.q) % self.q
+            pass
+        else:
+            l = (p2.y - p1.y) * inv(p2.x - p1.x, self.q) % self.q
+            pass
+        x = (l * l - p1.x - p2.x) % self.q
+        y = (l * (p1.x - x) - p1.y) % self.q
+        return Coord(x, y)
+ 
+    def mul(self, p, n):
+        """n times <mul> of elliptic curve
+        >>> m = ec.mul(p, n)
+        >>> assert ec.is_valid(m)
+        >>> assert ec.mul(p, 0) == ec.zero
+        """
+        r = self.zero
+        m2 = p
+        while 0 < n:
+            if n & 1 == 1:
+                r = self.add(r, m2)
+                pass
+            n, m2 = n >> 1, self.add(m2, m2)
+            pass
+        return r
+ 
+    def order(self, g):
+        """order of point g
+        >>> o = ec.order(g)
+        >>> assert ec.is_valid(a) and ec.mul(a, o) == ec.zero
+        >>> assert o <= ec.q
+        """
+        assert self.is_valid(g) and g != self.zero
+        for i in range(1, self.q + 1):
+            if self.mul(g, i) == self.zero:
+                return i
+            pass
+        raise Exception("Invalid order")
+    pass
+ 
+ 
+class DiffieHellman(object):
+    """Elliptic Curve Diffie Hellman (Key Agreement)
+    - ec: elliptic curve
+    - g: a point on ec
+    """
+    def __init__(self, ec, g):
+        self.ec = ec
+        self.g = g
+        self.n = ec.order(g)
+        pass
+ 
+    def gen(self, priv):
+        """generate pub key"""
+        assert 0 < priv and priv < self.n
+        return self.ec.mul(self.g, priv)
+ 
+    def secret(self, priv, pub):
+        """calc shared secret key for the pair
+        - priv: my private key as int
+        - pub: partner pub key as a point on ec
+        - returns: shared secret as a point on ec
+        """
+        assert self.ec.is_valid(pub)
+        assert self.ec.mul(pub, self.n) == self.ec.zero
+        return self.ec.mul(pub, priv)
+    pass
+ 
+
+ 
+ 
+if __name__ == "__main__":
+    print(" Enter file name")
+    name=raw_input()
+    #source = 'D://ecc/content.txt'
+
+    f = open('context.txt', 'r+')
+    txt = f.read()
+    print '\n Original file contents are :'
+    print txt
+    # shared elliptic curve system of examples
+    print "\n Enter curve parameters (a,b,q) y^2 = x^3 +ax + b (mod q)"
+    a=int(raw_input())#1
+    b=int(raw_input())#18
+    q=int(raw_input())#19
+    
+    ec = EC(a,b,q)
+    g, _ = ec.at(7)
+    assert ec.order(g) <= ec.q
+
+    # ECDH usage
+    dh = DiffieHellman(ec, g)
+    
+    
+    print("\n Enter private key for A")
+    apriv = int(raw_input()) #11   
+    apub = dh.gen(apriv)
+    print " Public key is",apub
+
+    print("\n Enter private key for B")
+    bpriv = int(raw_input())#3
+    bpub = dh.gen(bpriv)
+    print " Public key is",bpub
+
+    #cpriv = 7
+    #cpub = dh.gen(cpriv)
+    # same secret on each pair
+    assert dh.secret(apriv, bpub) == dh.secret(bpriv, apub)
+    a=(dh.secret(bpriv,apub))
+    print "\n Shared key is" , a
+BLOCK_SIZE = 32
+PADDING = '{'
+ 
+# one-liner to sufficiently pad the text to be encrypted
+pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
+ 
+# one-liners to encrypt/encode and decrypt/decode a string
+# encrypt with AES, encode with base64
+EncodeAES = lambda c, s: base64.b64encode(c.encrypt(pad(s)))
+DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(PADDING)
+ 
+# generate a random secret key and take its x
+x,y= dh.secret(bpriv,apub)
+secret=x+y
+print"\n Using x+y cordinate of shared key = " ,secret
+
+secret = hashlib.md5('secret').hexdigest()
+print "\n Calculating its md5 key = ", secret
+# create a cipher object using the random secret
+print"\n Now using AES and ECDH"
+cipher = AES.new(secret)
+ 
+# encode a string
+encoded = EncodeAES(cipher, txt)
+print '\n Encrypted text: \n', encoded
+ 
+# decode the encoded string
+decoded = DecodeAES(cipher, encoded)
+print '\n Decrypted text: \n', decoded
