5f7914d8bf1e8b9889d5be2f31a1ee03aa68a281
iCHAIT <cgupta319@gmail.com>
2015-04-17 23:01:22 +0530

    further work

---
+++ rsa/rsa.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import numpy
+import random
+from fractions import gcd
+import math
+import sympy
+
+a = raw_input("Enter a large prime no P:")
+
+b = raw_input("Enter another large prime no Q:")
+
+p = int(a)
+q = int(b)
+
+if not sympy.isprime(p):
+    print "ERROR p is not prime"
+    sys.exit()
+
+if not sympy.isprime(q):
+    print "ERROR q is not prime"
+    sys.exit()
+
+f = (p-1)*(q-1)
+
+n = p*q
+
+while 1:
+	e = random.randint(1, f) # Choosing a random no. e such that 1<e<f and e is coprime to f
+	if gcd(e, f) == 1:   # Condition for coprime
+		break
+
+for i in range(1, n-1):
+	if ((i * e) % f) == 1:
+		d = i         # d is private key.
+	break
+
+
+print "Public Key is:", e, n
+print "Private Key is:", d, n
