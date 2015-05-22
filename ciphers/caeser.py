74fec97dfc03989af154831fc338c013d074a168
iCHAIT <cgupta319@gmail.com>
2015-05-22 18:53:09 +0530

    minor change

--- ciphers/caeser.py
+++ ciphers/caeser.py
@@ -39,26 +39,10 @@ def decipher(message):
     print(plaintext)
 
 
-def bruteforce(message):
-    """
-       The inner loop does exactly the same thing as the decipher
-       function. The outer loop ensures it happens 26 times, changing
-       the key on each iteration.
-    """
-    for key in range(26):
-        plaintext = ""
-        for char in message:
-            plaintext += shift(char, key)
-        print(plaintext)
-
-
 def main():
-    """
-       Gets the message and the cipher method. The correct function
-       is selected from the OPTIONS_DICT constant.
-    """
     message = raw_input("Please enter text: ").upper().replace(" ", "")
-    method = raw_input("Please enter method, e for encipher, d for decipher, or b for bruteforce: ").upper()
+    method = raw_input(
+        "Please enter method, e for encryption, d for decryption.").upper()
     if method in OPTIONS_DICT:
         do_it = OPTIONS_DICT[method]
         do_it(message)
@@ -66,11 +50,8 @@ def main():
         print("Invalid option!")
 
 
-
-
 if __name__ == "__main__":
 
     OPTIONS_DICT = {'D': decipher,
-                    'E': encipher,
-                    'B': bruteforce}
+                    'E': encipher, }
     main()
